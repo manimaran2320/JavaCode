c16b682546d4e70f4e421414ee7c6dbd78bca953
manimaran2320 <130482765+manimaran2320@users.noreply.github.com>
2023-06-30 12:45:07 +0530

    Add files via upload

--- StringOperations.java
+++ StringOperations.java
@@ -5,51 +5,65 @@ public class StringOperations {
 		String s = "Mani";
 		String s1 = "Karthi";
 		// toUpperCase
-		System.out.println("toUpperCase : " + s.toUpperCase());
+//		System.out.println("toUpperCase : " + s.toUpperCase());
+//
+//		// toLowerCase
+//		System.out.println("toLowerCase : " + s1.toLowerCase());
+//
+//		// Concat
+//		System.out.println("concat : " + s.concat(" Karthi"));
+//
+//		// ==
+//		System.out.println("== : " + s == s1);
+//
+//		// equals()
+//		System.out.println("equals : " + s.equals(s1));
+//
+//		// charAt
+//		System.out.println("charAt : " + s.charAt(2));
+//
+//		// indexOf
+//		System.out.println("indexOf : " + s1.indexOf('K'));
+//
+//		// contains
+//		System.out.println("contains : " + s.contains("M"));
+//
+//		// replace
+//		System.out.println("replace : " + s.replace("Mani", "mANI"));
+//
+//		// trim
+//		System.out.println("Trim : " + s.trim());
+//
+//		// Split
+//		String str4 = "How are you?";
+//		String arrStr[] = str4.split(" ", 3);
+//		System.out.println("Splited Elements:");
+//		for (int i = 0; i < arrStr.length; i++) {
+//			System.out.print(arrStr[i] + " ");
+//		}
+//		System.out.println();
+//		System.out.println("Splited Elements using for each loop:");
+//		for (String str5 : arrStr) {
+//			System.out.print(str5 + " ");
+//		}
+//		String s3 = " ";
+//		System.out.println("isBlank : " + s3.isBlank());
+//		System.out.println("isEmpty : " + s3.isEmpty());
+//		System.out.println("Length : " + s3.length());
 
-		// toLowerCase
-		System.out.println("toLowerCase : " + s1.toLowerCase());
-
-		// Concat
-		System.out.println("concat : " + s.concat(" Karthi"));
-
-		// ==
-		System.out.println("== : " + s == s1);
-
-		// equals()
-		System.out.println("equals : " + s.equals(s1));
-
-		// charAt
-		System.out.println("charAt : " + s.charAt(2));
-
-		// indexOf
-		System.out.println("indexOf : " + s1.indexOf('K'));
-
-		// contains
-		System.out.println("contains : " + s.contains("M"));
-
-		// replace
-		System.out.println("replace : " + s.replace("Mani", "mANI"));
-
-		// trim
-		System.out.println("Trim : " + s.trim());
-
-		// Split
-		String str4 = "How are you?";
-		String arrStr[] = str4.split(" ", 3);
-		System.out.println("Splited Elements:");
-		for (int i = 0; i < arrStr.length; i++) {
-			System.out.print(arrStr[i] + " ");
+		// toCharArray
+		String s5="HTML CSS Java SQL";
+		char[] ch=s5.toCharArray();
+		String temp="";
+		for(int d: ch) {
+			
+			if(s5.isBlank()) {
+				break;
+			}
+			else
+				temp+=d;
+			System.out.println(temp);
 		}
-		System.out.println();
-		System.out.println("Splited Elements using for each loop:");
-		for (String str5 : arrStr) {
-			System.out.print(str5 + " ");
-		}
-		String s3 = "";
-		System.out.println("isBlank : " + s3.isBlank());
-		System.out.println("isEmpty : " + s3.isEmpty());
-		System.out.println("Length : " + s3.length());
 
 	}
 
