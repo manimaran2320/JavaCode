71a1cc1745168ee1dd0e6d3caa973c0ef11a2853
manimaran2320 <130482765+manimaran2320@users.noreply.github.com>
2023-07-04 19:52:36 +0530

    Add files via upload

---
+++ Alphabets.java
@@ -0,0 +1,19 @@
+package Assignments;
+
+public class Alphabets {
+	public static void main(String[] args) {
+		char i;
+		
+		//Lower case alphabets
+		for(i='a';i<='z';i++) {
+			System.out.print(i+" ");
+		}
+		System.out.println();
+		
+		//Upper case alphabets
+		for(i='A';i<='Z';i++) {
+			System.out.print(i+" ");
+		}
+	}
+
+}
---
+++ NaturalNums.java
@@ -0,0 +1,10 @@
+package Assignments;
+
+public class NaturalNums {
+	public static void main(String[] args) {
+		for(int i=0;i<=50;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
